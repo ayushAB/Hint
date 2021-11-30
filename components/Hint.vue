6ab400c67ea6f786ba3730aa2845828c1efbc80b
Ayush <ayushbante6@gmail.com>
2021-11-30 22:22:09 +0530

    hint demo

---
+++ components/Hint.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="relative">
+    <span
+      :class="`flex h-4 w-4 relative cursor-pointer ${rippleConfig.height}  ${rippleConfig.width}`"
+      @click="onHintClick"
+    >
+      <span
+        :class="`animate-ping absolute inline-flex h-full w-full rounded-full  opacity-75 ${rippleConfig.color}`"
+      ></span>
+      <span
+        :class="`relative inline-flex rounded-full h-4 w-4 ${rippleConfig.color}`"
+      ></span>
+    </span>
+    <diV
+      :class="`text-gray-800 absolute top-5 transition-opacity duration-200 ease-in-out left-0 z-50 rounded px-3 py-4  w-72 shadow-2xl ${
+        hintDailogConfig.backgroundColor
+      } ${showHint ? 'opacity-100' : 'opacity-0'} `"
+    >
+      <p :class="` ${hintDailogConfig.hintTextColor} `">
+        {{ hintDailogConfig.hintText }}
+      </p>
+      <div :class="`flex justify-${hintDailogConfig.buttonPostion}`">
+        <button
+          :class="`px-8 py-2 rounded ${hintDailogConfig.btnColor} ${hintDailogConfig.btnTextColor}`"
+          @click="onHintButtonClick"
+        >
+          Next
+        </button>
+      </div>
+    </diV>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    rippleConfig: {
+      type: Object,
+      default: () => {
+        return {
+          width: 'w-3',
+          height: 'h-3',
+          color: 'bg-green-400',
+        }
+      },
+    },
+    hintDailogConfig: {
+      type: Object,
+      default: () => {
+        return {
+          hintText: 'This is the Hint',
+          buttonPostion: 'end',
+          backgroundColor: 'bg-white',
+          btnColor: 'bg-indigo-600',
+          btnTextColor: 'text-white',
+          hintTextColor: 'text-gray-800',
+        }
+      },
+    },
+  },
+  data() {
+    return {
+      showHint: false,
+    }
+  },
+  methods: {
+    onHintClick() {
+      this.showHint = !this.showHint
+    },
+    onHintButtonClick() {
+      this.showHint = false
+      this.$emit('hint-button-click')
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="flex justify-center items-center h-full max-w-5xl mx-auto">
+    <div
+      class="flex flex-col justify-center flex-1 max-w-2xl shadow-2xl px-6 py-10 bg-indigo-600 gap-3 rounded"
+    >
+      <div
+        class="shadow-2xl p-3 bg-white rounded text-gray-700 font-bol text-center text-xl mb-10"
+      >
+        Hint Demo
+      </div>
+      <div>
+        <div class="flex items-center w-full">
+          <Hint v-if="hintNumber ==0 " :ripple-config="rippleConfig1" :hint-dailog-config="hintDailogConfig1" @hint-button-click="onHintDissmiss" />
+          <label for="price" class="block text-sm font-medium text-white ml-1"
+            >Price</label
+          >
+        </div>
+        <div class="mt-1 relative rounded-md shadow-sm">
+          <div
+            class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+          >
+            <span class="text-gray-500 sm:text-sm"> $ </span>
+          </div>
+          <input
+            id="price"
+            type="text"
+            name="price"
+            class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md h-10"
+            placeholder="0.00"
+          />
+          <div class="absolute inset-y-0 right-0 flex items-center">
+            <label for="currency" class="sr-only">Currency</label>
+            <select
+              id="currency"
+              name="currency"
+              class="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md"
+            >
+              <option>USD</option>
+              <option>CAD</option>
+              <option>EUR</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="flex items-center w-full">
+          <Hint v-if="hintNumber == 1" :hint-dailog-config="hintDailogConfig2" @hint-button-click="onHintDissmiss" />
+          <label for="price" class="block text-sm font-medium text-white ml-1"
+            >Price</label
+          >
+        </div>
+        <div class="mt-1 relative rounded-md shadow-sm">
+          <div
+            class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+          >
+            <span class="text-gray-500 sm:text-sm"> $ </span>
+          </div>
+          <input
+            id="price"
+            type="text"
+            name="price"
+            class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md h-10"
+            placeholder="0.00"
+          />
+          <div class="absolute inset-y-0 right-0 flex items-center">
+            <label for="currency" class="sr-only">Currency</label>
+            <select
+              id="currency"
+              name="currency"
+              class="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md"
+            >
+              <option>USD</option>
+              <option>CAD</option>
+              <option>EUR</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data (){
+    return {
+      hintNumber: 0,
+      rippleConfig1:{
+        height: 'h-3',
+        width: 'w-3',
+        color:'bg-gray-600'
+      },
+      hintDailogConfig1: {
+          hintText:'This is the Hint 2',
+          buttonPostion:  'end',
+          backgroundColor:  'bg-gray-800',
+          btnColor: 'bg-white',
+          btnTextColor:'text-gray-800',
+          hintTextColor:'text-white'
+        },
+        hintDailogConfig2: {
+          hintText:'This is the Hint 2',
+          buttonPostion:  'end',
+          backgroundColor:  'bg-indigo-800',
+          btnColor: 'bg-white',
+          btnTextColor:'text-gray-800',
+          hintTextColor:'text-white'
+        }
+    }
+  },
+
+  methods:{
+    onHintDissmiss() {
+      this.hintNumber++;
+    },
+  }
+}
+</script>
+<style>
+html,
+body {
+  height: 100%;
+}
+#__nuxt,
+#__layout {
+  height: 100%;
+  background-color: rgb(26, 24, 24);
+}
+</style>
